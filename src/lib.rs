ddb05f0c114f20e83787e63ffbf5374b3750c7fc
Howard Ricketts <molorosh@gmail.com>
2025-02-08 19:03:29 +0000

    added rot13 and basic punctuation

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,29 @@
 
 // the 'a notation helps to annotate the lifetime parameter so the 
 // compiler knows which method input is being borrowed from 
-pub fn tragmog<'a>(_text: &'a str, mode: &UnenglishMode) -> &'a str {
+pub fn transmogrify<'a>(_text: &'a str, mode: &UnenglishMode) -> &'a str {
+    let selected_index = match mode {
+        UnenglishMode::Diacritical => 0,
+        UnenglishMode::Japanglish => 1,
+        UnenglishMode::JuliusCaesar => 2
+    };
     match mode {
         UnenglishMode::Diacritical => "=D",
-        UnenglishMode::Japanglish => "=J"
+        UnenglishMode::Japanglish => "=J",
+        UnenglishMode::JuliusCaesar => "13"
     }
 }
+
+// default:     abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.?!
+// japanglish:  大乃匸占巳斤追井l子力レ瓜冂回尸只尺乙匕凵ソ山乂平弓大乃匸占巳斤追井l子力レ瓜冂回尸只尺乙匕凵ソ山乂平弓、。？！
+// diacritical: åƀçðèƒġħíĵķľṁñõþƣŗŝŧùʋŵχÿžÅßÇÐÈƑĠĦÍĴĶĽṀÑÕÞƢŖŜŦÙƲŴΧŸŽ,.?!
+// rot-13:     "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM.,!?"
+
 
 pub enum UnenglishMode {
     Japanglish,
     Diacritical,
+    JuliusCaesar
 }
 
 #[cfg(test)]
@@ -19,13 +32,19 @@ mod tests {
 
     #[test]
     fn it_works_for_japanglish() {
-        let result = tragmog("hello world", &UnenglishMode::Japanglish);
-        assert_eq!(result, "=J");
-    }
+        let result = transmogrify("Hello, World!", &UnenglishMode::Japanglish);
+        assert_eq!(result, "井巳レレ回, 山回尺レ占!");
+    } 
 
     #[test]
     fn it_works_for_diacritical() {
-        let result = tragmog("hello world", &UnenglishMode::Diacritical);
-        assert_eq!(result, "=D");
+        let result = transmogrify("Hello, World!", &UnenglishMode::Diacritical);
+        assert_eq!(result, "Ħèľľõ, Ŵõŗľð!");
+    }
+
+    #[test]
+    fn it_works_for_julius_caesar() {
+        let result = transmogrify("Hello, World!", &UnenglishMode::JuliusCaesar);
+        assert_eq!(result, "Olssv, Dvysk!");
     }
 }
